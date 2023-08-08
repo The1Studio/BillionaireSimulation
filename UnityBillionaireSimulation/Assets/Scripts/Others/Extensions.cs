87045b086eb581c3ff59faea1eb8b29ffb77b183
phuongdmp7 <phuongdmp7@gmail.com>
2023-08-08 15:51:28 +0700

    add format reward

--- UnityBillionaireSimulation/Assets/Scripts/Others/Extensions.cs
+++ UnityBillionaireSimulation/Assets/Scripts/Others/Extensions.cs
@@ -1,7 +1,14 @@
 ﻿namespace TheOneStudio.HyperCasual.Others
 {
+    using System.Globalization;
+
     public static class Extensions
     {
         public static int ToMilliseconds(this float second) { return (int)(second * 1000); }
+        public static string ToFormattedRewardCash(this long cash)
+        {
+            var result = cash.ToString("N0", new NumberFormatInfo { NumberGroupSeparator = "." });
+            return result;
+        }
     }
 }
