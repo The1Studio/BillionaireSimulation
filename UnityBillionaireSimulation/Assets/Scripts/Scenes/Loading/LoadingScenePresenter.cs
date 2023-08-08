ae6199b3018a6bae39cb9cd329005e0264e698c8
phuongdmp7 <phuongdmp7@gmail.com>
2023-08-08 15:27:18 +0700

    update min time to load

--- UnityBillionaireSimulation/Assets/Scripts/Scenes/Loading/LoadingScenePresenter.cs
+++ UnityBillionaireSimulation/Assets/Scripts/Scenes/Loading/LoadingScenePresenter.cs
@@ -2,9 +2,9 @@ namespace TheOneStudio.HyperCasual.Scenes.Loading
 {
     using BlueprintFlow.BlueprintControlFlow;
     using Core.AdsServices;
+    using Cysharp.Threading.Tasks;
     using GameFoundation.Scripts.AssetLibrary;
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
-    using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
     using GameFoundation.Scripts.Utilities.ObjectPool;
     using TheOneStudio.UITemplate.UITemplate.Scenes.Loading;
     using TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices;
@@ -14,8 +14,15 @@ namespace TheOneStudio.HyperCasual.Scenes.Loading
     [ScreenInfo("LoadingScreenView")]
     public class LoadingScenePresenter : UITemplateLoadingScreenPresenter
     {
+        private const int MinTimeToLoading = 3000;
         protected LoadingScenePresenter(SignalBus signalBus, UITemplateAdServiceWrapper adService, IAOAAdService aoaAdService, BlueprintReaderManager blueprintManager, UserDataManager userDataManager, IGameAssets gameAssets, ObjectPoolManager objectPoolManager) : base(signalBus, adService, aoaAdService, blueprintManager, userDataManager, gameAssets, objectPoolManager)
         {
+        }
+
+        protected override async UniTask Preload()
+        {
+            await base.Preload();
+            await UniTask.Delay(MinTimeToLoading);
         }
     }
 }
