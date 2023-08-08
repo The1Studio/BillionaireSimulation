c0b9816581dcb1a09808906dde6a00f60cadb5ae
khai1412 <khai14122002123@gmail.com>
2023-08-08 16:25:48 +0700

    update ranking sound

--- UnityBillionaireSimulation/Assets/Scripts/Scenes/Main/GameStateMachines/States/LeaderboardState.cs
+++ UnityBillionaireSimulation/Assets/Scripts/Scenes/Main/GameStateMachines/States/LeaderboardState.cs
@@ -7,16 +7,30 @@ using Zenject;
 
 namespace TheOneStudio.HyperCasual.Scenes.Main.GameStateMachines.States
 {
+    using GameFoundation.Scripts.Utilities;
+    using TheOneStudio.HyperCasual.Blueprints;
+
     public class LeaderboardState: BaseGameState
     {
         private readonly DiContainer   container;
         public override  GameStateType GameStateType { get; set; } = GameStateType.Leaderboard;
         public override  string        SegmentId     { get; set; }
+        
+        private readonly IAudioService      audioService;
+        private readonly MiscParamBlueprint miscParamBlueprint;
 
-        public LeaderboardState(ScreenHandler screenHandler, SignalBus signalBus, DiContainer container) : base(screenHandler, signalBus) { this.container = container; }
+        public LeaderboardState(ScreenHandler screenHandler, SignalBus signalBus, DiContainer container, IAudioService audioService, MiscParamBlueprint miscParamBlueprint) : base(screenHandler,
+            signalBus)
+        {
+            this.container          = container;
+            this.miscParamBlueprint = miscParamBlueprint;
+            this.audioService       = audioService;
+        }
 
         public override async void Enter()
         {
+            this.audioService.StopAllPlayList();
+            this.audioService.PlayPlayList(this.miscParamBlueprint.MusicWinAndRanking);
             await this.ScreenHandler.OpenPopup<LeaderBoardScreenPresenter, LeaderboardScreenModel>(new LeaderboardScreenModel()
             {
                 OnOkClicked = this.OnLeaderboardOkClicked
