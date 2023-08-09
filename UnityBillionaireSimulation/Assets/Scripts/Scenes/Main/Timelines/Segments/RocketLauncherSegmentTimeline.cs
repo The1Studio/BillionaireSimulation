705b188810ecd1022158712bcf5221f8baa254fe
phuongdmp7 <phuongdmp7@gmail.com>
2023-08-09 10:13:01 +0700

    wip add timeline signal and fix cam

---
+++ UnityBillionaireSimulation/Assets/Scripts/Scenes/Main/Timelines/Segments/RocketLauncherSegmentTimeline.cs
@@ -0,0 +1,21 @@
+﻿namespace TheOneStudio.HyperCasual.Scenes.Main.Timelines.Segments
+{
+    using TheOneStudio.HyperCasual.Scenes.Main.GamePlay.Views;
+    using UnityEngine;
+
+    public class RocketLauncherSegmentTimeline: SegmentTimeline
+    {
+        public void ChangeSkybox()
+        {
+            Debug.Log("Fuckkkkkkkkkkkkkkkk ChangeSkybox");
+            // var skybox = this.GetCurrentContainer().Resolve<ISkyboxService>();
+            // skybox.ChangeSkybox();
+        }
+        
+        public void AddStars()
+        {
+            // var skybox = this.GetCurrentContainer().Resolve<ISkyboxService>();
+            // skybox.AddStars();
+        }
+    }
+}
